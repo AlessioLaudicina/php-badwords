e17a46a081d967af42aad6bdae34dcfe1ba169ed
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-04-17 16:51:52 +0200

    aggiunto lunghezza stringa

--- badwords_form/badwords.php
+++ badwords_form/badwords.php
@@ -1,4 +1,7 @@
 <?php 
-$nome = $_GET['name'];
-echo $nome;
+$stringaA = $_GET['name'];
+$stringaB = $_GET['badword'];
+$lunghezzastringa = strlen($stringa);
+echo $stringa . '  Con una lunghezza complessiva di ' . $lunghezzastringa . '  caratteri';
+
 ?>
